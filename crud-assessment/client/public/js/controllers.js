03659f37b79a743c84a0a58382c2cb3dbca0a547
Luis <delapenl@gmail.com>
2015-09-25 11:36:22 -0600

    complete first phase

---
+++ crud-assessment/client/public/js/controllers.js
@@ -0,0 +1,78 @@
+app.controller('BeerController', function($scope, httpFactory, $timeout, $http) {
+  $scope.success = false;
+  $scope.beer = {};
+  $scope.edit = false;
+  getBeers = function(url) {
+    httpFactory.get(url)
+    .then(function(response) {
+      $scope.beers = response.data;
+  });
+};
+getBeers('api/v1/beers');
+
+function messageTimeout() {
+  $scope.success = false;
+}
+
+
+  $scope.postBeer = function() {
+    var payload = $scope.beer;
+    httpFactory.post('/api/v1/beers', payload)
+    .then(function(response){
+      $scope.beers.push(response.data);
+      $scope.beer = {};
+      $scope.success = true;
+      $scope.message = "Added a new beer! Woohoo!";
+      $timeout(messageTimeout, 5000);
+    });
+  };
+
+  $scope.deleteBeer = function(id) {
+        $http.delete('/api/v1/beer/' + id)
+        .then(function(response) {
+          console.log(response.data);
+          getBeers('api/v1/beers');
+        });
+      };
+
+  $scope.getBeer = function(id) {
+    $http.get('/api/v1/beer/' + id)
+    .then(function(response) {
+      console.log(response.data);
+      $scope.beerEdit = response.data;
+      });
+    $scope.edit = true;
+    console.log($scope.edit);
+  };
+
+
+  $scope.editBeer = function(id) {
+    console.log('test');
+    var payload = $scope.beerEdit;
+    $http.put('/api/v1/beer/' + id , payload)
+    .then(function(response) {
+    $scope.beerEdit.name = '';
+    $scope.beerEdit.type = '';
+    $scope.beerEdit.abv = '';
+    $scope.edit = false;
+    getBeers('api/v1/beers');
+
+
+    });
+  };
+
+});
+
+
+
+
+
+
+
+//     for (var i = 0; i < beers.length; i++) {
+//       if (name === beers[i].name) {
+//         $http.delete('api/v1/beer/:id')
+//           .then(function(response) {
+//         }
+//     };
+//   };
---
+++ crud-assessment/client/public/js/services.js
@@ -0,0 +1,18 @@
+app.factory("httpFactory", ['$http', function($http){
+  var obj = {};
+
+  // get request
+  obj.get = function(url) {
+    return $http.get(url);
+  };
+
+  obj.post = function(url, payload) {
+    return $http.post(url, payload);
+  };
+
+  // obj.delete = function(url) {
+  //   return $http.delete(url);
+  // };
+
+  return obj;
+}]);
